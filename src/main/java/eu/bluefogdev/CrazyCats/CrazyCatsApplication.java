3a96d20847e1be2d6b5b716366b92bf87cab2347
Sandro Simperl <sandro_simperl@infokom.info>
2023-10-22 08:34:30 +0200

    init project

---
+++ src/main/java/eu/bluefogdev/CrazyCats/CrazyCatsApplication.java
@@ -0,0 +1,13 @@
+package eu.bluefogdev.CrazyCats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyCatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyCatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/bluefogdev/CrazyCats/CrazyCatsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.bluefogdev.CrazyCats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyCatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
